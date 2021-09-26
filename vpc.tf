59ae7212cff40959bbfd63fb8951ff101d9f3acd
toyhoshi <marco.varagnolo@gmail.com>
2021-09-26 18:30:34 +0200

    EKS demo Hashicorp

--- vpc.tf
+++ vpc.tf
@@ -3,11 +3,11 @@ variable "region" {
   description = "AWS region"
 }
 
-provider "aws" {
-  region = var.region
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
-}
+# provider "aws" {
+#   region = var.region
+#   access_key = var.AWS_ACCESS_KEY_ID
+#   secret_key = var.AWS_SECRET_ACCESS_KEY
+# }
 
 terraform {
   backend "remote" {
